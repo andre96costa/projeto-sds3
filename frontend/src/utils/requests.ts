455ad775c895ef53911ea543614fda54ae6a9f41
Andre Costa <andre2396costa@gmail.com>
2021-05-07 14:18:33 -0300

    Outro teste com a var do netlify

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,3 +1,3 @@
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-export const BASE_URL = "https://sds3-andrecosta.herokuapp.com";
+export const BASE_URL = process.env.REACT_APP_BACKENDPROJECT ?? 'http://localhost:8080';
+//export const BASE_URL = "https://sds3-andrecosta.herokuapp.com";
 //export const BASE_URL = "http://localhost:8080";
